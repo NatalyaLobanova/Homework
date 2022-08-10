94d75e5afc039a8f5eb5c7458d20c0185fb4964c
Natalya Lobanova <knopka40a@mail.ru>
2022-08-10 17:50:48 +0500

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA);
+    Console.WriteLine("min = " + numberB);
+}
+else
+{
+    Console.WriteLine("max = " + numberB);
+    Console.WriteLine("min = " + numberA);
+}
